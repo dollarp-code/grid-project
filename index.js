1230ab71dd13bf6df800df94c9448d911a8da5f7
dolapo <husseindollarp@yahoo.com>
2024-01-28 16:35:55 -0800

    writing a little javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
+hamburger.addEventListener("click", () => {
+    bar1.classList.toggle("animatebar1")
+});
+hamburger.addEventListener("click", () => {
+    bar2.classList.toggle("animatebar2")
+});
+hamburger.addEventListener("click", () => {
+    bar3.classList.toggle("animatebar3")
+});
